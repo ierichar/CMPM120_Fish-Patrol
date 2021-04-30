2c7429884f8a32bf5276a44c4e8bcc910d441de6
Ian Richardson <ierichar@ucsc.edu>
2021-04-29 20:01:15 -0700

    added point breakdown

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,6 +5,17 @@
  * Estimated Completion Time: ~ 10 hours
  * 
  * Menu.js
+ * 
+ * S(hrek) Tier: (60) Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)
+ * - Replaced rocket with fisherman and fishing bob, as well as the spaceships with fish.
+ * - UI altered to match fishing theme
+ * - Hit sound on fish changed to splash
+ * Intermediate Tier: (20) Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
+ * - Implemented small goldfish worth 100 points (moves faster)
+ * Novice Tier: (10) Implement parallax scrolling
+ * - Implemented parallax scrolling with fish, sky and clouds
+ * Starting Tier: (5) Add your own (copyright-free) background music to the Play scene
+ * - Added copyright-free music from freemusic.org
  */
 class Menu extends Phaser.Scene {
     constructor() {
